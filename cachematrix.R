76fdd67e388d8bb875c8c9cc3e30f175316e9c0f
nomlet <nomlet@users.noreply.github.com>
2014-10-23 01:42:00 -0500

    My solution for Programming Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Inverting a matrix is an expensive operation.  As an alternative, hide
+## the matrix behind a set of functions which calculates the inverse only
+## when necessary and otherwise retrieves a cached copy.
 
-## Write a short comment describing this function
+## Generate a list of functions to serve as a "front" for the matrix, x, 
+## allowing it to be accessed via a cache.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) inv <<- inverse
+    getinverse <- function() inv
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)  
 }
 
 
-## Write a short comment describing this function
+## Solve for the inverse of a matrix via its "front" functions.  Only
+## calculate the inverse if it is not already available in the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinverse(inv)
+    inv
 }
